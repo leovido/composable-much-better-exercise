7687a74c230d09a0ed4e267448411b218b4daa23
Christian Ray Leovido <christian.leovido@gmail.com>
2021-10-20 14:06:34 +0100

    test: update snapshots

--- MuchBetterDependencies/Tests/LoginTests/LoginUITests.swift
+++ MuchBetterDependencies/Tests/LoginTests/LoginUITests.swift
@@ -5,7 +5,7 @@ import SwiftUI
 import XCTest
 
 final class LoginUITests: XCTestCase {
-    func testLoginUI() {
+    func testLoginUIEmpty() {
         let store: Store<LoginState, LoginAction> = .init(
             initialState: .init(),
             reducer: .empty,
@@ -16,4 +16,28 @@ final class LoginUITests: XCTestCase {
 
         assertSnapshot(matching: loginView, as: .image(on: .iPhoneXsMax))
     }
+
+	func testLoginUIEmail() {
+		let store: Store<LoginState, LoginAction> = .init(
+			initialState: .init(email: "user@muchbetter.com"),
+			reducer: .empty,
+			environment: LoginEnvironment.mock
+		)
+
+		let loginView = UIHostingController(rootView: LoginView(store: store))
+
+		assertSnapshot(matching: loginView, as: .image(on: .iPhoneXsMax))
+	}
+
+	func testLoginUIPassword() {
+		let store: Store<LoginState, LoginAction> = .init(
+			initialState: .init(password: "super-secure-password"),
+			reducer: .empty,
+			environment: LoginEnvironment.mock
+		)
+
+		let loginView = UIHostingController(rootView: LoginView(store: store))
+
+		assertSnapshot(matching: loginView, as: .image(on: .iPhoneXsMax))
+	}
 }
