c5e3565a75b22a5bbff33bfa26762eed8633eb4a
Christian Ray Leovido <christian.leovido@gmail.com>
2021-10-25 18:43:38 +0100

    Create Encoder+Extension.swift

---
+++ MuchBetterDependencies/Sources/Common/Helpers/Encoder+Extension.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//
+//
+//  Created by Christian Leovido on 25/10/2021.
+//
+
+import Foundation
+
+public extension JSONEncoder {
+    static let customEncoder: JSONEncoder = {
+        let encoder = JSONEncoder()
+        encoder.dateEncodingStrategy = .iso8601
+
+        return encoder
+    }()
+}
