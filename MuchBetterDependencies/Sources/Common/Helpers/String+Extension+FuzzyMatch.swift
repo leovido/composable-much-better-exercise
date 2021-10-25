7081586bc0ca19a17111db7348abbcb3498ef9ad
Christian Ray Leovido <christian.leovido@gmail.com>
2021-10-25 15:23:42 +0100

    feat: add fuzzy match for searching/filtering

---
+++ MuchBetterDependencies/Sources/Common/Helpers/String+Extension+FuzzyMatch.swift
@@ -0,0 +1,28 @@
+//
+//  File.swift
+//
+//
+//  Created by Christian Leovido on 25/10/2021.
+//
+
+import Foundation
+
+public extension String {
+    func fuzzyMatch(_ string: String) -> Bool {
+        let lowercased = string.lowercased()
+
+        let words = map { $0.lowercased() }
+            .joined()
+            .components(separatedBy: " ")
+
+        for (_, word) in words.enumerated() {
+            if word == lowercased || word.contains(lowercased) {
+                return true
+            } else {
+                continue
+            }
+        }
+
+        return false
+    }
+}
