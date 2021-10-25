704f966bb3eb555ca0a8128969337acd3a382381
Christian Ray Leovido <christian.leovido@gmail.com>
2021-10-25 18:44:00 +0100

    feat: add selected filter option and new loading state

--- MuchBetterDependencies/Sources/TransactionFeature/TransactionView.swift
+++ MuchBetterDependencies/Sources/TransactionFeature/TransactionView.swift
@@ -28,23 +28,27 @@ public struct TransactionView: View {
                         Button {
                             viewStore.send(.sortTransactions(.newToOld))
                         } label: {
-                            Label("Sort by newest", systemImage: "")
+                            Label("Sort by newest",
+                                  systemImage: viewStore.state.sort == .newToOld ? "checkmark.circle.fill" : "")
                         }
                         Button {
                             viewStore.send(.sortTransactions(.oldToNew))
                         } label: {
-                            Label("Sort by oldest", systemImage: "")
+                            Label("Sort by oldest",
+                                  systemImage: viewStore.state.sort == .oldToNew ? "checkmark.circle.fill" : "")
                         }
                         Divider()
                         Button {
                             viewStore.send(.sortTransactions(.lowHighPrice))
                         } label: {
-                            Label("Price low/high", systemImage: "")
+                            Label("Price low/high",
+                                  systemImage: viewStore.state.sort == .lowHighPrice ? "checkmark.circle.fill" : "")
                         }
                         Button {
                             viewStore.send(.sortTransactions(.highLowPrice))
                         } label: {
-                            Label("Price high/low", systemImage: "")
+                            Label("Price high/low",
+                                  systemImage: viewStore.state.sort == .highLowPrice ? "checkmark.circle.fill" : "")
                         }
                     } label: {
                         Image(systemName: "line.3.horizontal.decrease.circle")
@@ -58,9 +62,10 @@ public struct TransactionView: View {
                         HStack {
                             VStack(alignment: .leading) {
                                 Text(transaction.description)
-                                    .font(Font.system(size: 17, design: .serif))
+                                    .font(Font.system(size: 17, design: .rounded))
                                 Text(formatted(transaction.date))
                                     .font(.caption)
+                                    .foregroundColor(Color(UIColor.systemGray))
                             }
                             Spacer()
                             Text(transaction.amount)
@@ -68,7 +73,14 @@ public struct TransactionView: View {
                         }
                     }
                     .listStyle(InsetGroupedListStyle())
-                } else {
+                    .refreshable {
+                        viewStore.send(.fetchTransactions)
+                    }
+
+                } else if viewStore.viewState == .loading {
+                    ProgressView("Refresing transactions...")
+                        .progressViewStyle(CircularProgressViewStyle(tint: .orange))
+                } else if viewStore.viewState == .empty {
                     VStack {
                         Spacer()
                         Text("No transactions")
