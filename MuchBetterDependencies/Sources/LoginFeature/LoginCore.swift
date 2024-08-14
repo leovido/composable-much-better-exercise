40af97845372923b914bcfae535a95b2b4a6a16f
Christian Ray Leovido <18484997+kuriishu27@users.noreply.github.com>
2024-08-14 18:29:16 +0100

    fix: add login screen

--- MuchBetterDependencies/Sources/LoginFeature/LoginCore.swift
+++ MuchBetterDependencies/Sources/LoginFeature/LoginCore.swift
@@ -11,12 +11,14 @@ public struct Login: Reducer {
 		var isEmailValid: Bool?
 		var password: String = ""
 		var isPasswordValid: Bool?
+		var isLoading: Bool = false
 		
 		public init() {}
 	}
 	
-	public enum Action: Equatable {
+	public enum Action: Equatable, BindableAction {
 		case alert(PresentationAction<LoginAlert>)
+		case binding(BindingAction<State>)
 		case logout
 		case login
 		case loginResponse(Result<String, LoginError>)
@@ -41,83 +43,87 @@ public struct Login: Reducer {
 	public var body: some ReducerOf<Self> {
 		Reduce { state, action in
 			switch action {
-			case let .loginResponse(.failure(error)):
-				
-				state.alert = .init(
-					title: TextState("Error"),
-					message: TextState(error.localizedDescription),
-					dismissButton: .default(
-						TextState("Ok"),
-						action: .send(.dismiss)
+				case .binding:
+					return .none
+				case let .loginResponse(.failure(error)):
+					
+					state.alert = .init(
+						title: TextState("Error"),
+						message: TextState(error.localizedDescription),
+						dismissButton: .default(
+							TextState("Ok"),
+							action: .send(.dismiss)
+						)
 					)
-				)
-				
-				return .none
-				
-			case .logout:
-				return .run { send in
-					_ = try await loginClient.logout()
-				}
-			case let .responseEmailValidate(isEmailValid):
-				
-				state.isEmailValid = isEmailValid
-				
-				return .none
-				
-			case let .responsePasswordValidate(isPasswordValid):
-				
-				state.isPasswordValid = isPasswordValid
-				
-				return .none
-				
-			case let .emailValidate(email):
-				struct EmailCancelId: Hashable {}
-				
-				let isEmailValid = email.contains("@")
-				
-				return .run { send in
-					await send(.responseEmailValidate(isEmailValid))
-				}
-				.debounce(id: EmailCancelId(), for: 0.5, scheduler: self.mainQueue)
-				
-			case let .passwordValidate(password):
-				struct PasswordCancelId: Hashable {}
-				
-				let isPasswordValid = password.count >= 6
-				
-				return .run { send in
-					await send(.responsePasswordValidate(isPasswordValid))
-				}
-				.debounce(id: PasswordCancelId(), for: 0.5, scheduler: self.mainQueue)
-				
-			case .dismissLoginAlert:
-				state.alert = nil
-				
-				return .none
-				
-			case .login:
-				let email = state.email
-				let password = state.password
-				return .run { send in
-					let token = try await loginClient.login(email, password)
 					
-					// Simulate a login wait time of 2 seconds
-					try await Task.sleep(nanoseconds: 2_000_000_000)
+					return .none
 					
-					await send(.loginResponse(.success(token)))
-				} catch: { error, send in
-					await send(.loginResponse(.failure(.message(error.localizedDescription))))
-				}
-				
-			case .loginResponse:
-				return .none
-				
-			case .dismissAlert:
-				state.alert = nil
-				
-				return .none
-			case .alert:
-				return .none
+				case .logout:
+					return .run { send in
+						_ = try await loginClient.logout()
+					}
+				case let .responseEmailValidate(isEmailValid):
+					
+					state.isEmailValid = isEmailValid
+					
+					return .none
+					
+				case let .responsePasswordValidate(isPasswordValid):
+					
+					state.isPasswordValid = isPasswordValid
+					
+					return .none
+					
+				case let .emailValidate(email):
+					struct EmailCancelId: Hashable {}
+					
+					let isEmailValid = email.contains("@")
+					
+					return .run { send in
+						await send(.responseEmailValidate(isEmailValid))
+					}
+					.debounce(id: EmailCancelId(), for: 0.5, scheduler: self.mainQueue)
+					
+				case let .passwordValidate(password):
+					struct PasswordCancelId: Hashable {}
+					
+					let isPasswordValid = password.count >= 6
+					
+					return .run { send in
+						await send(.responsePasswordValidate(isPasswordValid))
+					}
+					.debounce(id: PasswordCancelId(), for: 0.5, scheduler: self.mainQueue)
+					
+				case .dismissLoginAlert:
+					state.alert = nil
+					
+					return .none
+					
+				case .login:
+					state.isLoading = true
+					let email = state.email
+					let password = state.password
+					return .run { send in
+						let token = try await loginClient.login(email, password)
+						
+						// Simulate a login wait time of 2 seconds
+						try await Task.sleep(nanoseconds: 2_000_000_000)
+						
+						await send(.loginResponse(.success(token)))
+					} catch: { error, send in
+						await send(.loginResponse(.failure(.message(error.localizedDescription))))
+					}
+					
+				case .loginResponse:
+					state.isLoading = false
+					return .none
+					
+				case .dismissAlert:
+					state.alert = nil
+					
+					return .none
+				case .alert:
+					return .none
 			}
 		}
 		.ifLet(\.$alert, action: \.alert)
--- MuchBetterDependencies/Sources/LoginFeature/LoginView.swift
+++ MuchBetterDependencies/Sources/LoginFeature/LoginView.swift
@@ -3,24 +3,63 @@ import SwiftUI
 
 public struct LoginView: View {
 	@Bindable public var store: StoreOf<Login>
-
+	
 	public init(store: StoreOf<Login>) {
-    self.store = store
-  }
-
-  public var body: some View {
+		self.store = store
+	}
+	
+	public var body: some View {
 		WithPerceptionTracking {
-      ProgressView("Logging you in...")
-        .progressViewStyle(CircularProgressViewStyle(tint: .orange))
-				.transition(.opacity)
-				.alert(
-					$store.scope(state: \.alert, action: \.alert)
-				)
-        .onAppear {
-          store.send(.login)
-        }
-    }
-  }
+			if store.isLoading {
+				ProgressView("Logging you in...")
+					.progressViewStyle(CircularProgressViewStyle(tint: .orange))
+					.transition(.opacity)
+					.alert(
+						$store.scope(state: \.alert, action: \.alert)
+					)
+			} else {
+				VStack {
+					Spacer()
+					VStack {
+						Text("MuchBetter app".uppercased())
+							.font(.largeTitle)
+							.fontWeight(.black)
+						
+						Text("with TCA (The Composable Architecture)")
+							.font(.caption)
+					}
+					.padding(.bottom, 50)
+					
+					TextField("Email", text: $store.email,
+										prompt: Text("user@muchbetter.com"))
+					.textFieldStyle(RoundedBorderTextFieldStyle())
+					.background(Color.white)
+					.keyboardType(.emailAddress)
+					.autocapitalization(.none)
+					
+					SecureField("Password", text: $store.password,
+											prompt: Text("········"))
+					.textFieldStyle(RoundedBorderTextFieldStyle())
+					
+					Button(action: {
+						store.send(.login)
+					}, label: {
+						Text("Login")
+							.foregroundColor(Color.white)
+					})
+					.padding(10)
+					.padding([.leading, .trailing], 15)
+					.background(Color.orange)
+					.clipShape(Capsule())
+					.padding()
+					
+					Spacer()
+				}
+				.padding()
+				.background(Color(UIColor.systemGray6))
+			}
+		}
+	}
 }
 
 #Preview {
